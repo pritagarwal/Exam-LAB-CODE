8c2853196e0961712e3ce0c2f412510e0c2a60c0
pritagarwal <pritagarwal2001@gmail.com>
2022-06-13 12:44:49 +0530

    Corrected Error in DFS(Using Iteration).c

--- GraphTraversal/DFS(Using Iteration).c
+++ GraphTraversal/DFS(Using Iteration).c
@@ -1,15 +1,19 @@
+/* File Input:-
+7
+0 1 1 0 1 0 0
+1 0 0 1 0 1 0
+1 0 0 1 0 0 1 
+0 1 1 0 1 0 0 
+1 0 0 1 0 0 0 
+0 1 0 0 0 0 0
+0 0 1 0 0 0 0 */
+
 #include <stdio.h>
 #include<stdlib.h>
+#define n 7
     int visited[7] = {0,0,0,0,0,0,0};
-    int A [7][7] = {
-        {0,1,1,1,0,0,0},
-        {1,0,1,0,0,0,0},
-        {1,1,0,1,1,0,0},
-        {1,0,1,0,1,0,0},
-        {0,0,1,1,0,1,1},
-        {0,0,0,0,1,0,0}, 
-        {0,0,0,0,1,0,0} 
-    };
+    int A [7][7] ;
+
     
     struct stack{
         int top;
@@ -45,32 +49,54 @@
         return d;
     }
     
-
-    void DFS_Itr(int i){
-        struct stack s;
-        s.top=0;
+    void DFS_ITR(int p){
+       struct stack s;
+        s.top=-1;
         s.size = 100;
         s.arr = (int*)malloc(s.size*sizeof(int));
-         push(&s,i);
-        visited[i] =1;
-         printf("\n%d ",i);
+         push(&s,p);
+        visited[p] = 1;
          while(isEmpty(&s)==0){
             
             int d = pop(&s);
+            printf(" %d ",d+1);
             for(int i=0;i<7;i++){
-                if(A[d][i]==1 && !visited[i])
+                if(A[d][i]==1 && visited[i] ==0)
                  {
-                     printf("%d ",i);
                      visited[i] = 1;
                      push(&s,i);
                  }
             }
          }
     }
+    
+
 
 int main()
 {
-    DFS_Itr(0);
+    FILE *fp;
+    fp = fopen("Ex.txt","r");
+    if(fp ==NULL)
+     printf("NULL");
+     
+    int n1;
+    fscanf(fp,"%d",&n1);
+
+
+    for(int i=0;i<n1;i++){
+        for(int j=0;j<n1;j++){
+            fscanf(fp,"%d",&A[i][j]);
+        }
+    }
+    for(int i=0;i<n1;i++){
+        for(int j=0;j<n1;j++){
+            printf("%d ",A[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\nThe Required DFS Traversal is :-");
+    DFS_ITR(0);
+    
 
     return 0;
 }
