695257ce1889eb6e7b16a5bbd4e5136b8626b9b7
pritagarwal <pritagarwal2001@gmail.com>
2022-06-13 15:29:24 +0530

    Added Shortest Path Function

--- DP/FloydWarshall.c
+++ DP/FloydWarshall.c
@@ -1,10 +1,9 @@
-/******************************************************************************
 
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
+/*4
+0 10 5 3 
+1 0 999 999 
+999 2 0 999 
+999 999 2 0 */
 
 #include <stdio.h>
 #define INF 999
@@ -12,17 +11,58 @@ Write your code in this editor and press "Run" button to compile and execute it.
 int g[4][4];
 int n;
 
+void print(int arr[4][4],int p[4][4]){
+    printf("\n");
+    for(int i=0 ;i<4;i++){
+        for(int j=0;j<4;j++){
+            if(arr[i][j] == INF)
+            printf("I ");
+            else
+            printf("%d ",arr[i][j]);
+        }
+        printf("\t");
+         for(int j=0;j<4;j++){
+             if(p[i][j] == -1)
+              printf("N ");
+              else
+            printf("%d ",p[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+
+void sortPath(int u,int v,int p[4][4]){
+    
+    if(u == v){
+        printf(" %d ",u+1);
+    }
+    else if(p[u][v] == -1){
+        printf("NO path exist");
+    }
+    else{
+        sortPath(u,p[u][v],p);
+        printf(" %d ",v+1);
+    }
+    
+    
+    
+}
+
 void floyd(){  
     
     int d[n][n];
+    int p[n][n];
   
     for(int i=0;i<n;i++){
         for(int j =0;j<n;j++){
              d[i][j] = g[i][j];
+             if(g[i][j] == 0 || g[i][j] == INF)
+              p[i][j] = -1;
+             else
+              p[i][j] = i;
         }
     }
-   
-    
     
     for(int k=0;k<n;k++){
         for(int i=0;i<n;i++){
@@ -31,29 +71,30 @@ void floyd(){
                     continue;
                 }
                 else{
-                    if(d[i][k]+d[k][j]<d[i][j])
-                     d[i][j] = d[i][k]+d[k][j];
+                    if(d[i][k]+d[k][j]<d[i][j]){
+                        d[i][j] = d[i][k]+d[k][j];
+                        p[i][j] = p[k][j];
+                    }
+                     
+                     
                 }
             }
         }
+        
+         print(d,p);
     }
-    
+       
+        printf("Enter the u v :- ");
+        int u, v;
+        scanf("%d %d",&u,&v);
+        sortPath(u-1,v-1,p);
+       
     for(int i=0;i<n;i++){
         if(d[i][i] <0){
             printf("Negative edge weight present");
             return;
         }
     }
-    printf("All shortest path:-\n");
-   for(int i=0;i<n;i++){
-       for(int j=0;j<n;j++){
-           if(d[i][j] == 0||d[i][j] == INF)
-           continue;
-           printf("%c  %c %d\n",i+65,j+65,d[i][j]);
-       }
-       printf("\n---------------\n");
-   }
-    
 }
 
 int main()
